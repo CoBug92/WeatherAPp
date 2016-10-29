b6af9202a2c9315d849223bdca631b99f54f4693
CoBug92 <b.kostyuchenko@gmail.com>
2016-10-29 15:40:56 +0200

    Initial commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Богдан Костюченко on 29/10/2016.
+//  Copyright © 2016 Богдан Костюченко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
