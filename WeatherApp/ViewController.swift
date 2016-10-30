b183fb78ec27944904ef5f92bcbae5c01caa6a41
CoBug92 <b.kostyuchenko@gmail.com>
2016-10-30 03:03:20 +0100

    Created fake data

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -13,18 +13,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var pressureLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
-    @IBOutlet weak var tamperatureLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var appearentTemperatureLabel: UILabel!
     @IBOutlet weak var refreshButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let icon = WeatherIconManager.Rain.image
+        
+        let currentWeather = CurrentWeather(temperature: 10, appearentTemperature: 5, humidity: 20, pressure: 750, icon: icon)
+        updateUIWith(currentWeather: currentWeather)
         
     }
+    
+    func updateUIWith(currentWeather: CurrentWeather) {
+        self.imageView.image = currentWeather.icon
+        //Look at an extension in CurrentWeather.swift
+        self.pressureLabel.text = currentWeather.pressureString
+        self.humidityLabel.text = currentWeather.humidityString
+        self.temperatureLabel.text = currentWeather.temperatureString
+        self.appearentTemperatureLabel.text = currentWeather.appearentTemperatureString
+    }
+    
     
     @IBAction func refreshButtonTapped(_ sender: UIButton) {
     }
     
 }
+
+
 
