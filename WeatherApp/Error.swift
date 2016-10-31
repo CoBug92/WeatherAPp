833f64ccff32b3ddf1ee6bafc80be2d8bdb33c5f
CoBug92 <b.kostyuchenko@gmail.com>
2016-10-31 12:11:25 +0100

    Protocol extension

---
+++ WeatherApp/Error.swift
@@ -0,0 +1,12 @@
+//
+//  Error.swift
+//  WeatherApp
+//
+//  Created by Богдан Костюченко on 31/10/2016.
+//  Copyright © 2016 Богдан Костюченко. All rights reserved.
+//
+
+import Foundation
+
+public let BOGNetworkingErrorDomain = "com.bogdan.WeatherApp.NetworkingError"
+public let MissingHTTPResponseError = 100
