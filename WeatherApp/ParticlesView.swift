c7cff345617431aa158be3f53371e6ed20943c40
CoBug92 <b.kostyuchenko@gmail.com>
2016-11-07 17:02:47 +0100

    Added Particles + SpriteKit Rain

---
+++ WeatherApp/ParticlesView.swift
@@ -0,0 +1,14 @@
+//
+//  ParticlesView.swift
+//  WeatherApp
+//
+//  Created by Богдан Костюченко on 07/11/2016.
+//  Copyright © 2016 Богдан Костюченко. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ParticlesView: SKView {
+
+}
