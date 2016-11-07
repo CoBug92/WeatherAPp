767e947d9c5b7f4ad5ab927e31c9c674cf01f9d5
CoBug92 <b.kostyuchenko@gmail.com>
2016-11-07 17:15:14 +0100

    Added rain effect

--- WeatherApp/ParticlesView.swift
+++ WeatherApp/ParticlesView.swift
@@ -10,5 +10,23 @@ import UIKit
 import SpriteKit
 
 class ParticlesView: SKView {
-
+    //метод для описания сцены
+    override func didMoveToSuperview() {
+        let scene = SKScene(size: self.frame.size)
+        scene.backgroundColor = UIColor.clear
+        self.presentScene(scene)
+        
+        //отключаем фон нашего SKView
+        self.allowsTransparency = true
+        self.backgroundColor = UIColor.clear
+        
+        //размещаем частицы на нашей сцене
+        if let particles = SKEmitterNode(fileNamed: "ParticlesScene.sks"){
+            particles.position = CGPoint(x: self.frame.size.width/2, y: self.frame.size.height)
+            //в каких пределах должны создаваться наши частицы
+            particles.position = CGVector(dx: self.bounds.size.width, dy: 0)
+            //добавляем particles на сцену
+            scene.addChild(particles)
+        }
+    }
 }
